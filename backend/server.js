c667646a7d08aee2ece5e6376b23e40befbf3327
aigitnet <aigitnet@gmail.com>
2025-08-26 12:14:06 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const { WebcastPushConnection } = require('tiktok-live-chat');
+const { TikTokLiveChat } = require('tiktok-live-stream-chat-reader');
 const app = express();
 const PORT = process.env.PORT || 3001;
- 
-let liveConnection = new WebcastPushConnection('YOUR_TIKTOK_USERNAME');
+
+let liveConnection = new TikTokLiveChat('YOUR_TIKTOK_USERNAME');
 const events = [];
- 
-liveConnection.connect().then(state => {
-  console.log(`Connected to TikTok Live. State: ${JSON.stringify(state)}`);
+
+liveConnection.connect().then(() => {
+  console.log(`Connected to TikTok Live.`);
 }).catch(err => {
   console.error('Failed to connect to TikTok Live.', err);
 });
- 
-liveConnection.on('chat', data => {
+
+liveConnection.on('comment', data => {
   events.push({
     type: 'comment',
-    data: { nickname: data.nickname, comment: data.comment }
+    data: { nickname: data.uniqueId, comment: data.comment }
   });
   if (events.length > 50) {
     events.shift();
   }
 });
- 
+
 app.get('/api/events', (req, res) => {
   res.json(events);
 });
- 
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
